899c0c17aeeaadf5db32d244fdc6a9878c547d56
Aris <Aris@Aris-PC>
2017-10-02 22:10:53 +0700

    Commit Kedua

--- src/id/co/array/Array.java
+++ src/id/co/array/Array.java
@@ -1,22 +1,21 @@
-package id.co.indocyber.oop;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package id.co.array;
 
 /**
  *
  * @author Aris
  */
-public class LatihanOOP {
+public class Array {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    int []genap={2,4,6,8,10};
     }
     
 }
