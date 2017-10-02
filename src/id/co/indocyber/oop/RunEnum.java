62009ed78e732879a368e05fade8e3af14bc59b9
Aris <Aris@Aris-PC>
2017-10-02 22:03:03 +0700

    Commit pertama

---
+++ src/id/co/indocyber/oop/RunEnum.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.co.indocyber.oop;
+
+/**
+ *
+ * @author Aris
+ */
+public class RunEnum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println(LatihanEnum.MINGGU);
+    }
+    
+}
